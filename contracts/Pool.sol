a440353920fd831b120670ccbd9d50e873ed243f
DesiredDesire <87488265+DesiredDesire@users.noreply.github.com>
2021-08-16 16:52:20 +0200

    Update contracts/Pool.sol
    Co-authored-by: Mikołaj Pniak <mmpniak@gmail.com>

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -98,7 +98,7 @@ contract DesireSwapV0Pool is Ticket, IDesireSwapV0Pool
 	function burn (address to, uint256 ticketID) external override{
 		address body = IDesireSwapV0Factory(factory).body(); 
 		(bool success, bytes memory data) = body.delegatecall(
-        	abi.encodeWithSignature("burn(aaddress to, uint256 ticketID)", to, ticketID)
+        	abi.encodeWithSignature("burn(address to, uint256 ticketID)", to, ticketID)
         );
 	}
 
