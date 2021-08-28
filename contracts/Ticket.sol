54981271868e2fdc22940982e97bc833be93157c
DesiredDesire <desireddesire@proton.com>
2021-08-28 17:02:11 +0200

    dodano kilka funckji view

--- contracts/Ticket.sol
+++ contracts/Ticket.sol
@@ -8,55 +8,83 @@ contract Ticket {
         int24 lowestRangeIndex;
         int24 highestRangeIndex;
         uint256 liqAdded;
-        //mapping (int24 => uint256) positionSupplyCoefficient;
     }
+    // Mapping from token Id to positionSupplyCoefficient
+    mapping (uint256 => mapping(int24 => uint256)) internal _ticketSupplyData;
+
 
     // Mapping from token ID to owner address
-    mapping (uint256 => address) internal _owners;
+    mapping (uint256 => address) private _owners;
 
     // Mapping owner address to token count
-    mapping (address => uint256) internal _balances;
+    mapping (address => uint256) private _balances;
 
     // Mapping from token ID to data
     mapping (uint256 => TicketData) internal _ticketData;
 
-    // Mapping from token Id to positionSupplyCoefficient
-    mapping (uint256 => mapping(int24 => uint256)) _ticketSupplyData;
 
-    uint256 emissionCounter;
+    uint256 private nextId;
 
     constructor() {
-        emissionCounter = 0;
+        nextId = 1;
     }
 
-    function numberOf(address owner) public view virtual returns (uint256) {
+    function getTicketOwner(uint256 ticketId) external view
+    returns(address)
+    {
+        return _owners[ticketId];
+    }
+
+    function getBalance(address owner) external view
+    returns(uint256)
+    {
+        return _balances[owner];
+    }
+
+    function findOwnedTickets(address owner, uint256 number) external view
+    returns (uint256)
+    {
+        require (number <= _balances[owner],"DSV0Tick(findOwnedTickets): number>_balances[owner]");
+        uint j = 1;
+        for( uint i = 1; i < nextId; i++){
+            if (_owners[i] == owner){
+                if(number == j++) return i;
+            }
+        }
+    }
+
+    function getTicketData(uint256 ticketId) external view
+    returns(TicketData memory)
+    {
+        return _ticketData[ticketId];
+    }
+
+    function numberOf(address owner) external view returns (uint256) {
         require(owner != address(0), "ERC721: ZERO_ADDRESS");
         return _balances[owner];
     }
 
-    function ownerOf(uint256 tokenId) public view virtual returns (address) {
+    function ownerOf(uint256 tokenId) public view returns (address) {
         address owner = _owners[tokenId];
         require(owner != address(0), "ERC721: TOKEN_DOES_NOT_EXIST");
         return owner;
     }
 
-    function _exists(uint256 tokenId) internal view virtual returns (bool) {
+    function _exists(uint256 tokenId) internal view returns (bool) {
         return _owners[tokenId] != address(0);
     }
 
-    function _mint(address to) internal virtual returns (uint256) {
+    function _mint(address to) internal returns (uint256) {
         require(to != address(0), "ERC721: ZERO_ADDRESS");
 
         _balances[to] += 1;
-        _owners[emissionCounter] = to;
-        emissionCounter++;
-        return (emissionCounter - 1);
+        _owners[nextId] = to;
+        return (nextId++);
     }
 
-    function _burn(uint256 tokenId) internal virtual {
+    function _burn(uint256 tokenId) internal {
         address owner = ownerOf(tokenId);
-
         _balances[owner] -= 1;
-        delete _owners[tokenId];
+        _owners[tokenId] = address(0);
     }
 }
