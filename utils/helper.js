0818b4243eafb1a2d2442cf3b0af7c59bcff09cb
miki <mmpniak@gmail.com>
2021-09-10 11:51:47 +0200

    cleanup variables

--- utils/helper.js
+++ utils/helper.js
@@ -1,32 +1,29 @@
 const fs = require("fs");
 const ethers = require("ethers");
 
-let cf, provider, deployer, accounts, ADDRESS_CONTRACT;
+const DEFAULT_GAS = { gasLimit: 4612388, gasPrice: '1000000000' };
+const ADDRESS = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
 
-const defaultGas = { gasLimit: 4612388, gasPrice: '1000000000' };
-const address = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
-
-cf = JSON.parse(fs.readFileSync("./artifacts/contracts/Factory.sol/DesireSwapV0Factory.json", "utf8"));
-provider = new ethers.providers.StaticJsonRpcProvider(
+const cf = JSON.parse(fs.readFileSync("./artifacts/contracts/Factory.sol/DesireSwapV0Factory.json", "utf8"));
+const provider = new ethers.providers.StaticJsonRpcProvider(
     'http://127.0.0.1:8545', { name: 'hardhat', chainId: 1337 }
 );
 
 const main = async() => {
-    accounts = await provider.listAccounts();
-    deployer = provider.getSigner(accounts[0]);
+    const accounts = await provider.listAccounts();
+    let deployer = provider.getSigner(accounts[0]);
 
     const contractFactory = new ethers.ContractFactory(cf.abi, cf.bytecode, deployer);
     const contract = await contractFactory.deploy(accounts[0]); // arg should be an actual poolbody address 
 
     await contract.deployed();
-    ADDRESS_CONTRACT = contract.address;
-    console.log("Contract deployed to:", ADDRESS_CONTRACT);
+    console.log("Contract deployed to:", contract.address);
 
     await contract
-        .createPool(accounts[1], accounts[2], 1000, { from: address, ...defaultGas })
+        .createPool(accounts[1], accounts[2], 1000, { from: ADDRESS, ...DEFAULT_GAS })
 
     await contract
-        .getPoolAddress(accounts[1], accounts[2], 1000, { from: address, ...defaultGas })
+        .getPoolAddress(accounts[1], accounts[2], 1000, { from: ADDRESS, ...DEFAULT_GAS })
         .then(res => console.log(res));
 }
 
