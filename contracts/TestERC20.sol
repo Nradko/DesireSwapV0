7aabfa41b72d4435cf98eb609487d9d9379472b6
Łukasz Łakomy <l.lakomy@webcon.pl>
2021-09-15 21:07:16 +0200

    add solidity prettier, add solhint and bind them to git commit

--- contracts/TestERC20.sol
+++ contracts/TestERC20.sol
@@ -3,12 +3,14 @@ pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract TestERC20 is ERC20
-{
-    constructor(string memory name_, string memory symbol_, address addr1, address addr2)
-    ERC20(name_, symbol_)
-    {
-        _mint(addr1, 10**18);
-        _mint(addr2, 10**18);
-    }
+contract TestERC20 is ERC20 {
+  constructor(
+    string memory name_,
+    string memory symbol_,
+    address addr1,
+    address addr2
+  ) ERC20(name_, symbol_) {
+    _mint(addr1, 10**18);
+    _mint(addr2, 10**18);
+  }
 }
--- contracts/interfaces/pool/IDesireSwapV0PoolActions.sol
+++ contracts/interfaces/pool/IDesireSwapV0PoolActions.sol
@@ -1,36 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-interface IDesireSwapV0PoolActions 
-{
-    function swap(
-        address to,
-        bool zeroForOne,
-        int256 amount,
-        uint256 sqrtPriceLimit,
-        bytes calldata data)
-    external
-	returns (int256, int256);
+interface IDesireSwapV0PoolActions {
+  function swap(
+    address to,
+    bool zeroForOne,
+    int256 amount,
+    uint256 sqrtPriceLimit,
+    bytes calldata data
+  ) external returns (int256, int256);
 
-    function mint(
-        address to,
-        int24 lowestRangeIndex,
-        int24 highestRangeIndex,
-        uint256 liqToAdd,
-		bytes calldata data)
-    external
-    returns(uint256 amount0, uint256 amount1);
+  function mint(
+    address to,
+    int24 lowestRangeIndex,
+    int24 highestRangeIndex,
+    uint256 liqToAdd,
+    bytes calldata data
+  ) external returns (uint256 amount0, uint256 amount1);
 
-    function burn(
-        address to,
-        uint256 ticketId)
-	external
-	returns (uint256, uint256);
+  function burn(address to, uint256 ticketId) external returns (uint256, uint256);
 
-    function flash(
-        address recipient,
-        uint256 amount0,
-        uint256 amount1,
-        bytes calldata data)
-    external;
+  function flash(
+    address recipient,
+    uint256 amount0,
+    uint256 amount1,
+    bytes calldata data
+  ) external;
 }
