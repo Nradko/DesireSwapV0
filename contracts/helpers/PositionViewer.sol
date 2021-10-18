1e9d520be574efd7e3a7d2755dccf6b1faec805e
DesiredDesire <desireddesire@proton.com>
2021-10-18 19:36:24 +0200

    Add position viewer test and small changes

--- contracts/helpers/PositionViewer.sol
+++ contracts/helpers/PositionViewer.sol
@@ -4,6 +4,8 @@ import '../interfaces/IDesireSwapV0Pool.sol';
 import '../interfaces/pool/ITicket.sol';
 import '../libraries/PoolHelper.sol';
 
+import 'hardhat/console.sol';
+
 contract PositionViewer {
 
     uint256 private constant D = 10**18;
@@ -18,13 +20,13 @@ contract PositionViewer {
         uint256 amount1;
     }
 
-    function getCurrentPrice(address poolAddress)
+    function getCurrentSqrtPrice(address poolAddress)
     public view
     returns(uint256){
         IDesireSwapV0Pool pool = IDesireSwapV0Pool(poolAddress);
         (uint256 reserve0, uint256 reserve1, uint256 sqrt0, uint256 sqrt1 , , ) = pool.getFullRangeInfo(pool.inUseRange());
         uint256 L = PoolHelper.LiqCoefficient(reserve0, reserve1, sqrt0, sqrt1);
-        return (L*L/(reserve1 + L*sqrt0/D));
+        return (L*D/(reserve1 + L*sqrt0/D));
     }
 
     function _readTicket(
@@ -75,17 +77,24 @@ contract PositionViewer {
 
     function getPositionDataList(address poolAddress, address owner)
     external view
-    returns (PositionData[] memory positionDataList, uint256 currentPrice){
+    returns (PositionData[] memory positionDataList){
         IDesireSwapV0Pool pool = IDesireSwapV0Pool(poolAddress);
         uint256 ticketAmount = pool.getAddressTicketsAmount(owner);
         uint256 positionCount;
-        for(uint256 i = 1; i <= ticketAmount; i++){
+        for(uint256 i = 1; i < ticketAmount; i++){
+            uint256 ticketId = pool.getAddressTickets(owner, i);
+            if(ticketId != 0) positionCount++;
+        }
+        positionDataList = new PositionData[](positionCount);
+        positionCount = 0;
+        for(uint256 i = 1; i < ticketAmount; i++){
             uint256 ticketId = pool.getAddressTickets(owner, i);
             if(ticketId != 0){
                 positionDataList[positionCount] = getPositionData(poolAddress, ticketId);
                 positionCount++;
             }
         }
-        currentPrice = getCurrentPrice(poolAddress);
+        return positionDataList;
+        //currentSqrtPrice = getCurrentSqrtPrice(poolAddress);
     }
 }
