e54a04bc6810b3380bdd72d41fe8d15c945f8f80
DesiredDesire <desireddesire@proton.com>
2021-10-21 16:56:47 +0200

    deadline check modyfication

--- contracts/LiquidityManager.sol
+++ contracts/LiquidityManager.sol
@@ -7,14 +7,41 @@ import './interfaces/IDesireSwapV0Pool.sol';
 
 import './base/PeripheryPayments.sol';
 import './base/PeripheryImmutableState.sol';
+import './base/PeripheryValidation.sol';
 
 import './interfaces/ILiquidityManager.sol';
 
 import './libraries/CallbackValidation.sol';
 import 'hardhat/console.sol';
 
-contract LiquidityManager is ILiquidityManager, PeripheryImmutableState, PeripheryPayments {
+contract LiquidityManager is ILiquidityManager, PeripheryImmutableState, PeripheryPayments, PeripheryValidation {
   constructor(address _factory, address _WETH9) PeripheryImmutableState(_factory, _WETH9) {}
+
+
+  ///////////
+  function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        while (_i != 0) {
+            k = k-1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+  ///////////
+
 
   struct MintCallbackData {
     PoolAddress.PoolKey poolKey;
@@ -37,6 +64,7 @@ contract LiquidityManager is ILiquidityManager, PeripheryImmutableState, Periphe
     external
     payable
     override
+    checkDeadline(params.deadline)
     returns (
       address poolAddress,
       uint256 ticketId,
@@ -44,14 +72,15 @@ contract LiquidityManager is ILiquidityManager, PeripheryImmutableState, Periphe
       uint256 amount1
     )
   {
-    require(block.number < params.deadline, 'DSV0LM(redeem): deadline');
     require(params.liqToAdd > 0, 'DSV0LM(supply): liquidity=0');
     PoolAddress.PoolKey memory poolKey = PoolAddress.getPoolKey(params.token0, params.token1, params.fee);
     poolAddress = PoolAddress.computeAddress(factory, poolKey);
     require(poolAddress != address(0), 'DSV0LM(supply): pool=0');
     IDesireSwapV0Pool pool = IDesireSwapV0Pool(poolAddress);
     (ticketId, amount0, amount1) = pool.mint(params.recipient, params.lowestRangeIndex, params.highestRangeIndex, params.liqToAdd, abi.encode(MintCallbackData({poolKey: poolKey, payer: msg.sender})));
-    require(amount0 <= params.amount0Max && amount1 <= params.amount1Max, 'DSV0LM(supply): amountMax_exceeded');
+    require(amount0 <= params.amount0Max, uint2str(amount0));
+    require(amount1 <= params.amount1Max, uint2str(amount1));
+
     ticketId = pool.getNextTicketId() - 1;
     emit Supply(params.recipient, poolAddress, ticketId);
   }
